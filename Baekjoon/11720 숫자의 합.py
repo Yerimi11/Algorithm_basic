c2af3fd3f8eeb3a77817f3316b7e9cb2a7aa9208
Yerimi11 <93559998+Yerimi11@users.noreply.github.com>
2022-02-06 01:02:43 +0900

    Update 11720 숫자의 합.py

--- Baekjoon/11720 숫자의 합.py
+++ Baekjoon/11720 숫자의 합.py
@@ -1,14 +1,28 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-m = input()
-li = []
-for i in range(len(m)-1):
-    li.append(m[i])
-
+m = input().rstrip()
 res = 0
-for i in li:
-    i = int(i)
-    res += i
-# res = sum(li[0:])
+for i in range(len(m)):
+    res += int(m[i])
+
+# res = 0
+# for i in li:
+#     i = int(i)
+#     res += i
 print(res)
+
+
+# import sys
+# input = sys.stdin.readline
+# n = int(input())
+# m = input()
+# li = []
+# for i in range(len(m)-1):
+#     li.append(m[i])
+
+# res = 0
+# for i in li:
+#     i = int(i)
+#     res += i
+# print(res)
