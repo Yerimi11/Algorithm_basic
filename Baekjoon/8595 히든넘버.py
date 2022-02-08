141b9d8d5a7a009fb1f00b9458a5a96ca4176bbe
Yerimi11 <93559998+Yerimi11@users.noreply.github.com>
2022-02-09 01:44:41 +0900

    Update 8595 히든넘버.py

--- Baekjoon/8595 히든넘버.py
+++ Baekjoon/8595 히든넘버.py
@@ -1,16 +1,39 @@
 import sys
 input = sys.stdin.readline
-n, mix = int(input()), input().rstrip()
-nums, temp = [], []
+n = int(input())
+mix = input().rstrip()
 
+nums, temp = [], []
 for i in mix:
     if i.isdigit() == True:
         temp.append(i)
+    
     else:
-        if len(temp) > 0:
-            nums.append(int(''.join(temp)))
-            temp = []
+        nums.append(int(''.join(temp)))
+        temp = []
 
+if len(temp) > 0:
+    nums.append(int(''.join(temp)))
 print(sum(nums))
 
 # 13으로 잘려야하는데 1, 3으로 잘려서 합이 29->20으로 출력돼서 오류남. -> join으로 해결
+# 3, 2a3 처럼 마지막에 숫자가 들어올 경우 고려
+
+# 5
+# 21a11b21
+
+import sys
+input = sys.stdin.readline
+n = int(input())
+mix = input().rstrip()
+
+nums, temp = 0, "0"
+for i in mix:
+    if i.isdigit() == True:
+        temp+=i
+    
+    else:
+        nums += int(temp)
+        temp = "0"
+
+print(nums+int(temp))
