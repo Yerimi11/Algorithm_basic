820996780594cce97b17f6689c74ad8bc5d0229b
Yerimi11 <93559998+Yerimi11@users.noreply.github.com>
2022-02-11 10:33:10 +0900

    Update 11728 배열합치기.py

--- Baekjoon/11728 배열합치기.py
+++ Baekjoon/11728 배열합치기.py
@@ -1,8 +1,10 @@
 import sys
 input = sys.stdin.readline
 n, m = map(int, input().split())
-# A = input().split()
+# A = input().split() <- str으로 들어옴 ['2', '3']
 # B = input().split()
+# 3과 17을 문자열로 받은 상태에서 정렬하면 17이 먼저 정렬되기 때문에 이러면 안 됨.
+
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 arr = ' '.join(map(str,sorted(A+B)))
