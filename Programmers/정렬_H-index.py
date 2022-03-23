05b6e3b038242f0ac53b2613d2e4e2c2a5f95426
yerim <sontt4266@naver.com>
2022-03-24 00:45:44 +0900

    0324

--- Programmers/정렬_H-index.py
+++ Programmers/정렬_H-index.py
@@ -1,16 +1,34 @@
 # 정렬을 안하게되면 시간복잡도는 n^2이 됨
 # 정렬 -> for문 -> i보다 큰 수가 몇 개 있는지 체크하고, 
-# 작은 수 == 전체 리스트 길이 - 큰 수 갯수 그 수를 return
+# 작은 수 == 전체 리스트 길이 - 큰 수 갯수 그 수를 return => 항상 같은 값 나오므로 의미X
+
+# def solution(citations):
+#     answer = []
+#     length = len(citations)
+#     citations.sort()    # [0, 1, 3, 5, 6]
+    
+#     for i in range(length-1):
+#         count = 0
+#         count += len(citations[i+1:length])
+#         if len(citations[0:i+1]) == length - count:
+#             answer.append(i)
+#     return max(answer)
+
+# 정렬을 안하게되면 시간복잡도는 n^2이 됨
+# 정렬 -> for문 -> i보다 큰 수가 몇 개 있는지 체크하고, 
+# 인용횟수(citations[i]) == 전체 리스트 길이 - 큰 수 갯수. 그 수를 return
 
 def solution(citations):
     answer = []
     length = len(citations)
     citations.sort()    # [0, 1, 3, 5, 6]
     
-    for i in range(length-1):
+    for i in range(length):
+        # if citations[i] == length - len(citations[i+1:length]):
         count = 0
         count += len(citations[i+1:length])
-        if len(citations[0:i+1]) == length - count:
-            answer.append(i)
+        if citations[i] <= (length - count) and citations[i] > len(citations[0:i]):
+            answer.append(citations[i])
+    print(answer)
     return max(answer)
         
