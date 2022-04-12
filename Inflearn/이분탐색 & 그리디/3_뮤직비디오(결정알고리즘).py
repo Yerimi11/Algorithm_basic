95fc84679a05f777eb50984643ad22b10dfed1e4
yerim <sontt4266@naver.com>
2022-04-12 17:30:54 +0900

    0412

--- Inflearn/이분탐색 & 그리디/3_뮤직비디오(결정알고리즘).py
+++ Inflearn/이분탐색 & 그리디/3_뮤직비디오(결정알고리즘).py
@@ -1,15 +1,15 @@
 import sys
 input = sys.stdin.readline
 n, m = map(int, input().split())
-minutes = list(int(input()) for _ in range(n))
+minutes = list(map(int, input().split()))
 
 def Count(capacity):
     cnt = 1
     sum = 0
     for x in minutes:
-        if sum+x > capacity:
+        if sum + x > capacity:
             cnt += 1
-            sum = x
+            sum = x 
         else:
             sum += x
     return cnt
@@ -17,9 +17,11 @@ def Count(capacity):
 l, r = 1, sum(minutes)
 res = 0
 
+maxx = max(minutes)
 while l <= r:
     mid = (l+r) // 2
-    if Count(mid) <= m:
+    # 가장 긴 노래보다 DVD 하나의 용량이 크거나 같아야하며, ~
+    if mid >= maxx and Count(mid) <= m:
         res = mid
         r = mid - 1
     else:
