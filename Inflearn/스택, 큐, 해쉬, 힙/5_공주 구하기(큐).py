b8843bc3f409efdad215e2dc36cd83bcae0464ab
yerim <sontt4266@naver.com>
2022-04-23 01:14:00 +0900

    0423

--- Inflearn/스택, 큐, 해쉬, 힙/5_공주 구하기(큐).py
+++ Inflearn/스택, 큐, 해쉬, 힙/5_공주 구하기(큐).py
@@ -1,15 +1,17 @@
 import sys
+from collections import deque
 sys.stdin = open("/Users/yerim/Downloads/pythonalgorithm_formac/스택,큐,해쉬,힙/5. 공주구하기/in5.txt", 'r')
 # input = sys.stdin.readline
 n, k = map(int, input().split())
-queue = []
+queue = list(range(1, n+1))
+queue = deque(queue)
 
-for i in range(n):
-    queue.append(i+1)
+# for i in range(n):
+#     queue.append(i+1)
 
 while len(queue) > 1:
     for i in range(k):
-        queue.append(queue.pop(0))
+        queue.append(queue.popleft())
         if i == k-1:
             queue.pop()
 
