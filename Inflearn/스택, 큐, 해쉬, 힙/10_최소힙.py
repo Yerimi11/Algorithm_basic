b46ba979305cab1b21f431d8772f8b67c94f0e27
yerim <sontt4266@naver.com>
2022-04-24 17:47:30 +0900

    0424

--- Inflearn/스택, 큐, 해쉬, 힙/10_최소힙.py
+++ Inflearn/스택, 큐, 해쉬, 힙/10_최소힙.py
@@ -1,6 +1,15 @@
 import sys
 import heapq as hq
 sys.stdin=open("/Users/yerim/Downloads/pythonalgorithm_formac/스택,큐,해쉬,힙/10. 최소힙/in1.txt", "r")
-a = []
+nums = []
 while True:
-    
+    n = int(input())
+    if n == -1:
+        break
+    if n == 0:
+        if len(nums) == 0:
+            print(-1)
+        else:
+            print(hq.heappop(nums))
+    else:
+        hq.heappush(nums, n)
