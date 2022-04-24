c120f56c2dc2d97d0392d02f25c767527a2d6489
yerim <sontt4266@naver.com>
2022-04-24 15:27:33 +0900

    0424

---
+++ Inflearn/스택, 큐, 해쉬, 힙/8_단어 찾기(해쉬).py
@@ -0,0 +1,18 @@
+import sys
+# sys.read=open("/Users/yerim/Downloads/pythonalgorithm_formac/스택,큐,해쉬,힙/8. 단어찾기/in1.txt", 'r')
+input = sys.stdin.readline
+n = int(input())
+# 해쉬문제
+poetry = dict()
+# 단어들을 key값으로, val를 1로 설정해둔 후, 쓰인 단어들의 val를 0으로 바꿔 체크
+for i in range(n):
+    word = input()
+    poetry[word] = 1
+for i in range(1-1):
+    word = input()
+    poetry[word] = 0
+
+for key, val in poetry.items():
+    if val == 1:
+        print(key)
+        break
