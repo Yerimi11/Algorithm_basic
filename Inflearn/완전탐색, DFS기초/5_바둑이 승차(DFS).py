5cd56e3f7daf088f8da8ea3eb7959c547f02eaa0
yerim <sontt4266@naver.com>
2022-04-25 19:38:20 +0900

    0425

--- Inflearn/완전탐색, DFS기초/5_바둑이 승차(DFS).py
+++ Inflearn/완전탐색, DFS기초/5_바둑이 승차(DFS).py
@@ -1,25 +1,51 @@
 import sys
 sys.stdin=open("/Users/yerim/Downloads/pythonalgorithm_formac/완전탐색,DFS기초/5. 바둑이 승차/in5.txt", "r")
 
-def DFS(L, sum):
+# def DFS(L, sum):
+#     if sum > c:
+#         return
+#     if L == n:
+#         if sum < c:
+#             temp.append(sum)
+#             return
+#     else: 
+#         DFS(L+1, sum + dogs[L])
+#         DFS(L+1, sum)
+
+# if __name__ == "__main__":
+#     c, n = map(int, input().split())
+#     sum = 0
+#     temp = []
+#     dogs = []
+#     for i in range(n):
+#         dogs.append(int(input()))
+#     DFS(1, dogs[0])
+#     print(max(temp))
+    
+
+# 속도 개선
+def DFS(L, sum, temp_sum):
+    global result
+    if sum + (total-sum) < result:   # cut edge
+        return
     if sum > c:
         return
     if L == n:
-        if sum < c:
-            temp.append(sum)
-            return
+        if sum > result:
+            result = sum
     else: 
-        DFS(L+1, sum + lst[L])
-        DFS(L+1, sum)
+        DFS(L+1, sum + dogs[L], temp_sum + dogs[L])
+        DFS(L+1, sum, temp_sum + dogs[L])
 
 if __name__ == "__main__":
     c, n = map(int, input().split())
     sum = 0
-    temp = []
-    lst = []
+    result = -2147000000
+    temp_sum = 0
+    dogs = []
     for i in range(n):
-        lst.append(int(input()))
-    DFS(1, lst[0])
-    print(max(temp))
-    
-
+        dogs.append(int(input()))
+        temp_sum += dogs[i]
+    total = temp_sum
+    DFS(1, dogs[0], 0)
+    print(result)
