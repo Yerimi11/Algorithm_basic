02a9654f6c02a3b55ab1c569b2e072d886c1be31
yerim <sontt4266@naver.com>
2022-04-28 14:32:36 +0900

    0428

---
+++ Inflearn/완전탐색, DFS기초/15_경로탐색(DFS).py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline()
+
+def DFS(v):
+    pass
+
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    g = [[0] * (n+1) for _ in range(n+1)]
+    visited = [0] * (n+1)
+    for i in range(m):
+        a, b = map(int, input().split())
+        g[a][b] = 1
+    cnt = 0
+    visited[1] = 1
+    DFS(1)
