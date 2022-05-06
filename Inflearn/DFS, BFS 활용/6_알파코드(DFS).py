5fea21aef8ddeaaf6991daa1921938ce5ebee907
yerim <sontt4266@naver.com>
2022-05-06 15:48:17 +0900

    0506

--- Inflearn/DFS, BFS 활용/6_알파코드(DFS).py
+++ Inflearn/DFS, BFS 활용/6_알파코드(DFS).py
@@ -10,18 +10,23 @@ def DFS(L):
         if len(res) > len(num):
             res = res[2:]   # pop, slicing 둘 다 O(N). pop(0)을 2번 하면, 2N이니까 슬라이싱 사용. pop(0)을 O(1)로 하려면 deque사용하면 됨
         print("".join(res))
+        # print(res)
         cnt += 1
         return
     a = int(num[L])
     b = int("".join(num[L:L+2]))
-
+    if a == 0:
+        return
     if 0 < a < 10:
         res.append(chr(a+64))
+        # res.append(a)
         DFS(L+1)
+        res.pop()
     if 9 < b < 27:
-        res = res[:-2]
         res.append(chr(b+64))
+        # res.append(b)
         DFS(L+2)
+        res.pop()
     
 if __name__ == "__main__":
     num = list(str(input()).rstrip())
