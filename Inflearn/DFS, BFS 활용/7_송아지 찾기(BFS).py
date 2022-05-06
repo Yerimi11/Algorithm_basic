38a99a23db99794983d03a38cb19f305167aa35e
yerim <sontt4266@naver.com>
2022-05-06 19:59:33 +0900

    0506

--- Inflearn/DFS, BFS 활용/7_송아지 찾기(BFS).py
+++ Inflearn/DFS, BFS 활용/7_송아지 찾기(BFS).py
@@ -2,21 +2,50 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-def BFS(L):
-    if Q[-1] == e:
-        print((L//3)+1)
-    else:
-        a = Q.popleft()
-        if a-1 > 0:
-            Q.append(dis[a-1])
-        Q.append(dis[a+1])
-        if a+5 <= e:
-            Q.append(dis[a+5])
-        BFS(L+1)
+MAX = 10000
+ch = [0] * (MAX+1)
+dis = [0] * (MAX+1)
+n, m = map(int, input().split())
+ch[n] = 1
+dis[n] = 0
+dQ = deque()
+dQ.append(n)
 
-if __name__ == "__main__":
-    s, e = map(int, input().split())
-    dis = list(range(0, e+1))
-    Q = deque()
-    Q.append(s)
-    BFS(0)
+while dQ:
+    now = dQ.popleft()
+    if now == m:
+        break
+    for next in (now-1, now+1, now+5):
+        if 0 <= next <= MAX:
+            if ch[next] == 0:
+                dQ.append(next)
+                ch[next] = 1
+                dis[next] = dis[now]+1
+print(dis[m])
+
+# def BFS(L):
+#     if Q[-1] == e:
+#         print((L//3)+1)
+#     else:
+#         a = Q.popleft()
+#         # if a-1 > 0:
+#         Q.append(a-1)
+#         if dis[a-1] < L: 
+#             dis[a-1] += 1
+        
+#         Q.append(a+1)
+#         if dis[a+1] < L:
+#             dis[a+1] += 1
+#         if a+5 <= e:
+#             Q.append(a+5)
+#             if dis[a+5] < L:
+#                 dis[a+5] += 1
+        
+#         BFS(L+1)
+
+# if __name__ == "__main__":
+#     s, e = map(int, input().split())
+#     dis = [0] * (e+1)
+#     Q = deque()
+#     Q.append(s)
+#     BFS(0)
