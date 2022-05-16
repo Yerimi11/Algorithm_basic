818ff34e8a0f039fe854c3638642d422253baf02
yerim <sontt4266@naver.com>
2022-05-16 20:44:21 +0900

    0516

--- Inflearn/DFS, BFS 활용/15_토마토(BFS).py
+++ Inflearn/DFS, BFS 활용/15_토마토(BFS).py
@@ -4,41 +4,34 @@ input = sys.stdin.readline
 
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
-def BFS(x, y):
-    dQ.append((x, y))
-    visited[x][y] = True
-
+n, m = map(int, input().split())
+tomato = [list(map(int, input().split())) for _ in range(m)]
+dQ = deque()
+dis = [[0]*n for _ in range(m)]
+for i in range(m):
+    for j in range(n):
+        if tomato[i][j] == 1:
+            dQ.append((i, j))
+while dQ:
+    x, y = dQ.popleft()
     for i in range(4):
-        xx = x + dx[i]
-        yy = y + dy[i]
-        if tomato[xx][yy] == -1 and visited[xx][yy] == False:
-            visited[xx][yy] = True
-        if 0 <= xx < col and 0 <= yy < row and tomato[xx][yy] == 0 and visited[xx][yy] == False:
-            visited[xx][yy] = True
-            dQ.append((xx, yy))
-    
-
-if __name__ == "__main__":
-    row, col = map(int, input().split())
-    tomato = [list(map(int, input().split())) for _ in range(col)]
-    visited = [([False]*row) for _ in range(col)]
-    dQ = deque()
-    day = 0
-    if 0 not in tomato:
-        print(0)
-        sys.exit()
-
-    else:
-        while x < col and y < row:
-            for i in range(col):
-                for j in range(row):
-                    if tomato[i][j] == 1:
-                        BFS(i, j)
-            while dQ:
-                x, y = dQ.popleft()
-                tomato[x][y] = 1
-            day += 1
-        if 0 not in tomato:
-            print(day)
-        else:
-            print(-1)
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < m and 0 <= ny < n and tomato[nx][ny] == 0:
+            tomato[nx][ny] = 1
+            dis[nx][ny] = dis[x][y]+1
+            dQ.append((nx, ny))
+flag = 1
+for i in range(m):
+    for j in range(n):
+        if tomato[i][j] == 0:
+            flag = 0
+result = 0
+if flag == 1:
+    for i in range(m):
+        for j in range(n):
+            if dis[i][j] > result:
+                result = dis[i][j]
+    print(result)
+else:
+    print(-1)
