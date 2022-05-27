f242f0e3efd1ef224c4df1f52afaa28a220a8b0b
yerim <sontt4266@naver.com>
2022-05-27 22:04:33 +0900

    kill me

--- Line_2019.py
+++ Line_2019.py
@@ -1,34 +1,41 @@
 # https://engineering.linecorp.com/ko/blog/2019-firsthalf-line-internship-recruit-coding-test/
 import sys
 from collections import deque
-# input = sys.stdin.readline
-# c, b = map(int, input().split())
-c, b = 11, 2 # 3
-# 반례1) c, b = 11, 1 # 6
-# 반례2) c, b = 6, 3  # 4
+# c, b = 11, 2 # 5
+c, b = 11, 1 # 6 미해결 -> 짝/홀수로 나누기
+# c, b = 6, 3  # 4
 Brown = []
 ch = [0]*200001
+dis = [0]*200001
 Brown = deque(Brown)
 n = 0
-# ch[b] = 0
+dis[b] = 1
+temp = b
 Brown.append(b)
 
 while Brown:
     now = Brown.popleft()
+    if now > 200000:
+        print(-1)
+        break
+
+    ch[now] = 1
     if now == c:
         break
 
     for next in (now-1, now+1, now*2):
-        if 0 <= next <= 200000:
-            if ch[next] == 0:
-                Brown.append(next)
-                ch[next] = ch[now]+1
-    if ch[next] > n+1: # 여기서 트리 높이에 따라 값이 바뀌는 타이밍이 어긋나있음
+        if 0 <= next <= 200000 and ch[next] >= 0:
+            Brown.append(next)
+            ch[next] = -1
+            dis[next] = dis[now]+1
+            
+    if temp not in Brown:
         n += 1
-        c = c+n
-        # c = c + (ch[next] * (ch[next]+1)//2)
+        c = c + n
+        # c = c + (dis[now]*(dis[now]+1)//2)
+        temp = next
 
-print(ch[now]-1)             
+print(dis[now]-1)             
 
 
 
