11948ed58f5572262d01c30090ed32a2598ed1dc
yerim <sontt4266@naver.com>
2022-05-27 14:02:54 +0900

    0527

--- Programmers/Lab1.py
+++ Programmers/Lab1.py
@@ -1,21 +1,12 @@
 def solution(p, s):
-    res = 0
     sum = 0
-    for x, y in p, s:
-        # for y in s:
-            # 역/정방향 둘 다 계산해보고 더 적은 횟수로 채택
-            x, y = int(x), int(y)
-            a = abs(y-x)         # 정방향
-            b = abs((x*10)-y)    # 역방향
-            if a > b:
-                sum += b
-            else:
-                sum += a
-            pass
-
+    # 역/정방향 둘 다 계산해보고 더 적은 횟수로 채택
+    for i in range(len(p)):
+        a = abs(int(p[i])-int(s[i]))  # 정방향
+        b = abs(10-a)       # 역방향
+        sum += min(a, b)
     print(sum)
-    return res
-
+    return sum
 
 solution("82195", "64723") # 13
-# solution("00000000000000000000", "91919191919191919191") # 20
+solution("00000000000000000000", "91919191919191919191") # 20
