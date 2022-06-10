586ff7edfc47bb79808f18cf8df75e5879c1bb0a
yerim <sontt4266@naver.com>
2022-06-10 14:21:45 +0900

    0610

--- Programmers/Lv.1/Idea/숫자 문자열과 영단어.py
+++ Programmers/Lv.1/Idea/숫자 문자열과 영단어.py
@@ -1,17 +1,24 @@
-# isdecimal 사용해서 숫자와 문자 구분,
-# 해쉬테이블(딕셔너리)로 {one:1} 식으로 저장
-# str = ""해서 숫자 추가해준다
+def solution(s):
+    # isdecimal 사용해서 숫자와 문자 구분,
+    # 해쉬테이블(딕셔너리)로 {one:1} 식으로 저장
+    # str = ""해서 숫자 추가해준다
+    dict = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
+    num = ""
+    eng = ""
+    for x in s:
+        if x.isdecimal() == True:
+            num += x
+            if eng in dict:
+                num += dict[eng]     
+                eng = ""
+        else:
+            eng += x    
+        print("num : ", num)
+        print("eng : ", eng)
+        if eng in dict:
+            num += str(dict[eng])
+            eng = ""
 
-# 이게 카카오 문제라면 숨은 반례가 꽤 많지 않을까?
-# 생각보다 바로 답이 안나올 수도 있다.
-
-# 제한사항에 보니, return의 범위가 20억이다
-# -> 상관 없는 듯
+    return int(num)
 
 # enumerate로 푸는 방법도 해보자
-
-def solution(s):
-    answer = 0
-    return answer
-    
-    
