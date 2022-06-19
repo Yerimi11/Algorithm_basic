9e90ce5030a27617823954266a613f718b07d206
yerim <sontt4266@naver.com>
2022-06-19 17:53:29 +0900

    0619

--- Programmers/Lv.2/Idea/거리두기 확인하기.py
+++ Programmers/Lv.2/Idea/거리두기 확인하기.py
@@ -1,53 +1,45 @@
-def solution(places):
-    result = []
-    # 2차원 그래프로 하나씩 for문 돌며 str으로 입력한다.
-    graph = []
-    temp_p = []
-    temp_x = []
-    for k in places:
-        for i in k:
-            for j in i:
-                # 그러면 3중for문을 돌렸을 때 좌표값을 알게 된다.
-                graph.append(str(j))
-                # 돌리면서, P가 나오면 temp에 P의 좌표값을 저장해놓고,
-                if j == 'P':
-                    temp_p.append((i, j))
-                    # 다음 줄에서 P가 나왔을 때의 좌표값과 맨해튼 거리를 계산해서
-                    if len(temp_p) > 1:
-                        dis = abs(int(temp_p[0][0])-int(temp_p[1][0])+abs(int(temp_p[0][1])-int(temp_p[1][1]))
-                    # 3 미만이고, 위아래행에 X가 없었다면 return result.append(0)
-                        if len(temp_x) > 0:
-                            if dis < 3 and temp_x[0][1] != temp_p[0][1]:
-                                return result.append(0)
+
+
+
+
+
+
+
+# def solution(places):
+#     result = []
+#     # 2차원 그래프로 하나씩 for문 돌며 str으로 입력한다.
+#     graph = []
+#     temp_p = []
+#     temp_x = []
+#     for k in places:
+#         for i in k:
+#             for j in i:
+#                 # 그러면 3중for문을 돌렸을 때 좌표값을 알게 된다.
+#                 graph.append(str(j))
+#                 # 돌리면서, P가 나오면 temp에 P의 좌표값을 저장해놓고,
+#                 if j == 'P':
+#                     temp_p.append((i, j))
+#                     # 다음 줄에서 P가 나왔을 때의 좌표값과 맨해튼 거리를 계산해서
+#                     if len(temp_p) > 1:
+#                         dis = abs(int(temp_p[0][0])-int(temp_p[1][0]))+abs(int(temp_p[0][1])-int(temp_p[1][1]))
+#                         # 3 미만이고, 위아래행에 X가 없었다면 return result.append(0)
+#                         if len(temp_x) > 0:
+#                             if dis < 3 and temp_x[0][1] != temp_p[0][1]:
+#                                 return result.append(0)
                             
-                # 다음 P를 찾는 동안 X가 나오면 X좌표값도 저장하고 바로 다음 줄로 넘어간다
-                elif j == 'X':
-                    temp_x.append((i, j))
-            # 다 패스하면 return result.append(1)
-            return result.append(1)
-                        
-                
-                
-                
-    print(graph)
+#                 # 다음 P를 찾는 동안 X가 나오면 X좌표값도 저장하고 바로 다음 줄로 넘어간다
+#                 elif j == 'X':
+#                     temp_x.append((i, j))
+#             # 다 패스하면 return result.append(1)
+#             return result.append(1)
+                                        
+#     print(graph)
 
-    
-
-    return result
+#     return result
 
 # |r1-r2|+|c1-c2|인 맨해튼 거리가 3이상이거나,
 # 자리 사이에 파티션이 있으면 OK
-
-# Idea
-# 2차원 그래프로 하나씩 for문 돌며 str으로 입력한다.
-# 그러면 2중for문을 돌렸을 때 좌표값을 알게 된다.
-    # 돌리면서, P가 나오면 temp에 P의 좌표값을 저장해놓고,
-    # 다음 P를 찾는 동안 X가 나오면 X좌표값도 저장하고 바로 다음 줄로 넘어간다
-    # 다음 줄에서 P가 나왔을 때의 좌표값과 맨해튼 거리를 계산해서
-    # 3 미만이고, 위아래행에 X가 없었다면 return result.append(0)
-    # 다 패스하면 return result.append(1)
     
-
 
 # [["POOOP", 
 #   "OXXOX", 
@@ -60,3 +52,4 @@ def solution(places):
 #  ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], 
 #  ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], 
 #  ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]
+
