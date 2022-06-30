b8fda306d97d6d0aa3294afa522d79eb7d6b3337
yerim <sontt4266@naver.com>
2022-06-30 18:05:48 +0900

    0630

---
+++ Baekjoon/우선순위 큐/13334 철로.py
@@ -0,0 +1,27 @@
+# 8개, 길이 30선분
+정렬           5~35    10~40
+ 5 40         걸침     걸침
+10 20         O       O
+10 25         O       O
+30 25         O       O
+30 50         걸침     걸침
+35 25         O       O
+50 60         X       X
+80 100        X       X
+              4개     4개
+이걸 우선순위큐로..?
+튜플로 집어넣어서 최대힙으로 정렬시키고
+h1이 d1보다 크나 같고, o1이 d2보다 작거나 같을 때 cnt+=1
+else: 걸쳐지거나 d선분에 포함되지 않는 튜플은 힙에서 삭제
+모든 roads의 원소를 순회하고 힙의 최대길이를 출력
+
+import sys
+import heapq as hq
+input = sys.stdin.readline
+n = int(input())
+li = []
+for i in range(n):
+    s, e = map(int, input().split())
+    hq.heappush(li, (s, e))
+
+
