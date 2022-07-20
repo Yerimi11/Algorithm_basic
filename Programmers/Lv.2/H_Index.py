6b282ca537da8164654337aabd83df5ddb72d4e8
yerim <sontt4266@naver.com>
2022-07-20 23:59:46 +0900

    0720

--- Programmers/Lv.2/H_Index.py
+++ Programmers/Lv.2/H_Index.py
@@ -3,13 +3,11 @@
 # 인용횟수(citations[i]) == 전체 리스트 길이 - 큰 수 갯수. 그 수를 return
 
 def solution(citations):
-    answer = 0
     n = len(citations)
-    citations.sort()    # [0, 1, 3, 5, 6]
+    citations.sort()
     
     for i in range(n):
-        hIndex = n-i # H-index
+        hIndex = n-i
         if citations[i] >= hIndex:
-            answer = hIndex
-            break
-    return answer
+            return hIndex
+    return 0
