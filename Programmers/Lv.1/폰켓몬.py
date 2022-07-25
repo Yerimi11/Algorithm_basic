a30dbc20c07265170a7db5c472b2e0bffdc10767
yerim <sontt4266@naver.com>
2022-07-25 17:18:51 +0900

    0725

--- Programmers/Lv.1/폰켓몬.py
+++ Programmers/Lv.1/폰켓몬.py
@@ -1,18 +1,11 @@
-# 그리디인듯
-# 조합 이용?
-
-# n/2 구하고, kinds = len(set(nums)) 한 다음에
-# if kinds >= n/2: 
-    # return n/2
-# else:
-    # return kinds
-# => 이 방법으로 통과! (그리디)
-
 def solution(nums):
     mid = len(nums)//2
-    kinds = len(set(nums))
+    kinds = len(set(nums)) 
     if kinds >= mid: 
         return mid
     else:
         return kinds
 
+# 조합 이용 x
+# set도 해쉬자료형이다. 딕셔너리에서 key값만 있다고 보면 됨.
+# 해쉬, set 모두 O(1)
