6a1e29fc1f9cd102527ae0a6110a46e172d43cf8
yerim <sontt4266@naver.com>
2022-08-03 02:50:26 +0900

    0803

--- Programmers/Lv.3/디스크 컨트롤러.py
+++ Programmers/Lv.3/디스크 컨트롤러.py
@@ -1,25 +1,30 @@
+# 설명해볼 수 있나요?
+
 import heapq
 
 def solution(jobs):
-    answer, now, i = 0, 0, 0
-    start = -1 
+    worktime_sum, worktime, jobs_idx = 0, 0, 0 # worktime: 작업 시점
+    start_work = -1 
     heap = []
     
-    while i < len(jobs):
-        # 현재 시점에서 처리할 수 있는 작업을 heap에 저장
-        for j in jobs:
-            if start < j[0] <= now:
-                heapq.heappush(heap, [j[1], j[0]])
+    while jobs_idx < len(jobs): # jobs를 전부 탐색할 때 까지
+        for job in jobs:
+            # 현재 시점(시간)에서 처리할 수 있는 job을 heap에 저장
+            if start_work < job[0] <= worktime:
+                # 작업소요시간(job[1])을 기준으로 우선순위 큐 정렬
+                heapq.heappush(heap, [job[1], job[0]])
         
-        if len(heap) > 0: # 처리할 작업이 있는 경우
-            cur = heapq.heappop(heap)
-            start = now
-            now += cur[0]
-            answer += now - cur[1] # 작업 요청시간부터 종료시간까지의 시간 계산
-            i +=1
-        else: # 처리할 작업이 없는 경우 다음 시간을 넘어감
-            now += 1
+        # 처리할 job이 있는 경우
+        if len(heap) > 0: 
+            curr = heapq.heappop(heap)
+            start_work = worktime
+            worktime += curr[0] # 작업소요시간
+            # 총 작업 시간 합계 = 종료시간 - 작업 요청 시간
+            worktime_sum += worktime - curr[1] 
+            jobs_idx +=1
+        else: # 처리할 작업이 없는 경우 다음 시점(시간)으로 넘어감
+            worktime += 1
                 
-    return answer // len(jobs)
+    return worktime_sum // len(jobs)
 
 solution([[0, 3], [1, 9], [2, 6]]) # 9
