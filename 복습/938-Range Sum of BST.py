109ff3c4e1de5ecfb78028574b67ff3d97fd7607
yerim <sontt4266@naver.com>
2022-08-29 23:08:32 +0900

    0829

---
+++ 복습/938-Range Sum of BST.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/range-sum-of-bst/
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rangeSumBST(self, root, low, high):
+        """
+        :type root: TreeNode
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+        # low와 high를 포함하여 그 사이에 있는 값을 모두 더한 값을 출력한다
+        # 교재 p.423
+        def dfs(node):
+            if not node:
+                return 0
+            if node.val < low:
+                return dfs(node.right)
+            elif node.val > high:
+                return dfs(node.left)
+            return node.val + dfs(node.left) + dfs(node.right)
+        
+        return dfs(root)
