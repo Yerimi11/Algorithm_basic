7545d57c7002a189ebaf0a8bad811fc0e8d94b47
yerim <sontt4266@naver.com>
2022-08-29 17:09:33 +0900

    0829

--- Leetcode/Chap14_Tree/108_정렬된 배열의 이진 탐색 트리 변환/108-Convert Sorted Array to Binary Search Tree.py
+++ Leetcode/Chap14_Tree/108_정렬된 배열의 이진 탐색 트리 변환/108-Convert Sorted Array to Binary Search Tree.py
@@ -1,5 +1,8 @@
 
 # 오름차순 정렬이 된 상태로 주어진다.
+# len//2로 중앙 index를 구해서, 중앙인덱스값을 노드로 하고, 슬라이스를 통해
+# 왼쪽노드들 = [:mid], 오른쪽노드들 = [mid+1:] 로 이진트리를 구성한다.
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
