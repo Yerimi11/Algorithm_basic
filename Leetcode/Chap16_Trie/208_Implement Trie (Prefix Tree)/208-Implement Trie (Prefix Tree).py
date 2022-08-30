ea519acd333cfb34205719cfccd416c7f84399fb
yerim <sontt4266@naver.com>
2022-08-30 20:01:42 +0900

    0830

---
+++ Leetcode/Chap16_Trie/208_Implement Trie (Prefix Tree)/208-Implement Trie (Prefix Tree).py
@@ -0,0 +1,40 @@
+class TrieNode:
+    def __init__(self):
+        self.word = False
+        self.children = collections.defaultdict(TrieNode)
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            node = node.children[char]
+        node.word = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.word
+        
+    # 문자열로 시작 단어 존재 여부 판별
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
