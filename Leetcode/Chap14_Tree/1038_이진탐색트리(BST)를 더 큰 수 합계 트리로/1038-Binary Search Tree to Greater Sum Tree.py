594560cbcf44eeafbb13cbd1ffa6a7c2d4508bfd
yerim <sontt4266@naver.com>
2022-08-30 15:32:52 +0900

    0830

---
+++ Leetcode/Chap14_Tree/1038_이진탐색트리(BST)를 더 큰 수 합계 트리로/1038-Binary Search Tree to Greater Sum Tree.py
@@ -0,0 +1,61 @@
+# https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    greater_sum = 0
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        # 오른쪽 자식부터 운행하는 중위 순회
+        if not root.val:
+            return null 
+        
+        if root.right:
+            a = self.bstToGst(root.right)
+            self.greater_sum += a
+            return self.greater_sum
+        
+        self.greater_sum += root.val
+        # return self.greater_sum
+        
+        if root.left:
+            a = self.bstToGst(root.left)
+            self.greater_sum += a
+            return self.greater_sum
+        
+        # if root:
+        #     self.bstToGst(root.right)
+        #     self.greater_sum += root.val
+        #     root.val = self.greater_sum
+        #     self.bstToGst(root.left)
+        # return root
+
+
+# # Definition for a binary tree node.
+# # class TreeNode(object):
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+# class Solution(object):
+#     def bstToGst(self, root):
+#         """
+#         :type root: TreeNode
+#         :rtype: TreeNode
+#         """
+#         # 현재 값보다 더 큰 값을 가진 모든 노드의 값의 합을 구한다 (자기 자신 값 포함)
+#         # 자기 자신의 값을 포함한 우측 노드의 값의 합을 구한다
+#         # BST의 우측 노드는 항상 부모노드보다 값이 크다는 규칙이 있기 때문
+#         # 중위순회 : 오른쪽-부모-왼
+        
+#         # self.val : 지금까지 누적된 값 / root.val : 현재 노드의 값
+#         # 중위순회를 하면서 현재 노드의 값을 자기 자신을 포함한 지금까지의 누적된 값과 합한다
+        
+#         if root:
+#             self.bstToGst(root.right)
+#             self.val += root.val
+#             root.val = self.val
+#             self.bstToGst(root.left)
+#         return root
