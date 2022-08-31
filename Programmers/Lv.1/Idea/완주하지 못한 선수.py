5dfc08e2c1f8d0b9819dc13387e824fb910aa7d3
yerim <sontt4266@naver.com>
2022-08-31 09:44:14 +0900

    0830

--- Programmers/Lv.1/Idea/완주하지 못한 선수.py
+++ Programmers/Lv.1/Idea/완주하지 못한 선수.py
@@ -2,6 +2,17 @@
 # 완주한 명단은 참가자 명단에서 key값 찾아 value -1 해준다
 # 값이 0이 아닌 참가자의 key(이름)을 return 
 
-def solution():
-    answer = 0
+from collections import defaultdict
+def solution(participant, completion):
+    answer = ''
+    attendance = defaultdict(int)
+    for name in participant:
+        attendance[name] += 1
+    
+    for name in completion:
+        attendance[name] -= 1
+    
+    for name, num in attendance.items():
+        if attendance[name] > 0:
+            answer += name
     return answer
