b9b26ee366ce9d4bbe66fd5ae84c589fe2846c7f
yerim <sontt4266@naver.com>
2022-09-16 23:57:47 +0900

    0916

--- Programmers/Lv.2/후보키.py
+++ Programmers/Lv.2/후보키.py
@@ -3,3 +3,26 @@
 # [["a", "aa"], ["aa", "a"], ["a", "a"]]  >>>> 1
 # [["100", "100", "ryan", "music", "2"], ["200", "200", "apeach", "math", "2"], ["300", "300", "tube", "computer", "3"], ["400", "400", "con", "computer", "4"], ["500", "500", "muzi", "music", "3"], ["600", "600", "apeach", "music", "2"]] >>>> 3
 # [["a", "1", "aaa", "c", "ng"], ["b", "1", "bbb", "c", "g"], ["c", "1", "aaa", "d", "ng"], ["d", "2", "bbb", "d", "ng"]]  >>>>> 3
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/42890#
+
+def solution(relation):
+    answer_list = list()
+    for i in range(1, 1 << len(relation[0])):
+        tmp_set = set()
+        for j in range(len(relation)):
+            tmp = ''
+            for k in range(len(relation[0])):
+                if i & (1 << k):
+                    tmp += str(relation[j][k])
+            tmp_set.add(tmp)
+
+        if len(tmp_set) == len(relation):
+            not_duplicate = True
+            for num in answer_list:
+                if (num & i) == num:
+                    not_duplicate = False
+                    break
+            if not_duplicate:
+                answer_list.append(i)
+    return len(answer_list)
